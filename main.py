e7c43fa57beb6f5386f17d584ead3864159dee22
hernanxho <pixelcreathor@gmail.com>
2024-10-19 15:49:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
-class Grafo:
-    def __init__(self):
-        self.vertices = {}
+#import Graph as g
+#import pandas as pd
 
-    def agregar_vertice(self, vertice):
-        if vertice not in self.vertices:
-            self.vertices[vertice] = []
-
-    def agregar_arista(self, origen, destino):
-        if origen in self.vertices and destino in self.vertices:
-            self.vertices[origen].append(destino)
-            self.vertices[destino].append(origen)
-
-    def obtener_vertices(self):
-        return list(self.vertices.keys())
-    
-    def obtener_aristas(self):
-        aristas = []
-        for vertice, adyacentes in self.vertices.items():
-            for adyacente in adyacentes:
-                aristas.append((vertice, adyacente))
-        return aristas
-    
-
-grafo = Grafo()
-
-grafo.agregar_vertice("A")
-grafo.agregar_vertice("B")
-grafo.agregar_vertice("C")
-
-grafo.agregar_arista("A", "B")
-grafo.agregar_arista("A", "C")
-grafo.agregar_arista("B", "C")
-
-vertices = grafo.obtener_vertices()
-print("Vértices:", vertices)
-aristas = grafo.obtener_aristas()
-print("Aristas:", aristas)
+#map = g.Graph.__new__()
+#map = 3
+#print (type(map))
+print ("Hola mundo")
